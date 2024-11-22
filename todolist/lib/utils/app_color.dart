23795dfabf0c493de3e447c2dac467e2e2069d57
Mayur-Thinkitive <mayur.patil@thinkitve.com>
2024-11-22 23:11:31 +0530

    Revert "TODOLIST setup initialise"
    This reverts commit 5f8be7eabdf6437b0e7ea890161428ebb6bda496.

--- todolist/lib/utils/app_color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors{
-  static const Color primaryColor = Colors.blueGrey;
-  static const List<Color> primaryGradiantColor = [
-    Colors.blueGrey,
-    Colors.brown
-  ];
-}
