5f8be7eabdf6437b0e7ea890161428ebb6bda496
Mayur-Thinkitive <mayur.patil@thinkitve.com>
2024-11-22 23:10:14 +0530

    TODOLIST setup initialise

---
+++ todolist/lib/utils/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor = Colors.blueGrey;
+  static const List<Color> primaryGradiantColor = [
+    Colors.blueGrey,
+    Colors.brown
+  ];
+}
